ed1b8a6495d456dcae7ab80cf18d14e814d4bfb2
N-Pauwels <164620051+N-Pauwels@users.noreply.github.com>
2024-03-23 11:47:32 +0100

    update

---
+++ sortinghatScipt.js
@@ -0,0 +1,9 @@
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const button5 = document.querySelector("#button5");
+const button6 = document.querySelector("#button6");
+const text = document.querySelector("#text");
+const hatText = document.querySelector("#hatText");
+const questions
