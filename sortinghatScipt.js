eff008f4b40de1f95e964ef0e2a73ec5767842a8
N-Pauwels <164620051+N-Pauwels@users.noreply.github.com>
2024-03-25 23:12:30 +0000

    UPDATE

--- sortinghatScipt.js
+++ sortinghatScipt.js
@@ -15,6 +15,7 @@ let ravenclawPoints = 0;
 let currentQuestion = 0;
 const buttons = [button1, button2, button3, button4, button5, button6]
 const choiceText = ["Good choice!", "Lovely!", "Merlin's beard!","You're a wizard!","Butterbeer's on me!", "Don't let the muggles get you down!","You have chosen wisely!","The hat approves.","I solemnly swear that's a fine choice.", "You're awesome!", "That's worth a chocolate frog!", "Bloody hell!", "The hat knew you'd say that.","What a great answer!"]
+//define questions
 const questions = [
     {
         name: "question1",
